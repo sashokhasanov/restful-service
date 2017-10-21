b87c1e77e6555a33c3ebb11240e828dbd62b27db
Alexander Khasanov <sashokhasanov@mail.ru>
2017-10-21 17:00:44 +0300

    Application service configuration added

--- src/main/java/ru/khasanov/rest/Main.java
+++ src/main/java/ru/khasanov/rest/Main.java
@@ -3,20 +3,28 @@ package ru.khasanov.rest;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import ru.khasanov.rest.manage.AccountManager;
+import ru.khasanov.rest.manage.TransactionManager;
+import ru.khasanov.rest.storage.AccountStorage;
+import ru.khasanov.rest.storage.TransactionStorage;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * Main class.
+ * Main application class.
  *
+ * @author Aleksandr Khasanov
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://localhost:8080/rest/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     *
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
@@ -31,15 +39,40 @@ public class Main {
 
     /**
      * Main method.
-     * @param args
-     * @throws IOException
+     *
+     * @param args array of {@link String} arguments.
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+
+        initApplicationService();
+
         final HttpServer server = startServer();
+
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            server.shutdownNow();
+        }
+    }
+
+    private static void initApplicationService()
+    {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        AccountStorage accountStorage = new AccountStorage();
+        AccountManager accountManager = new AccountManager(accountStorage, executorService);
+        accountManager.setTimeout(1000);
+        ApplicationService.getInstance().initAccountManager(accountManager);
+
+        TransactionStorage transactionStorage = new TransactionStorage();
+        TransactionManager transactionManager  = new TransactionManager(transactionStorage, accountStorage, executorService);
+        transactionManager.setTimeout(1000);
+        ApplicationService.getInstance().initTransactionManager(transactionManager);
     }
 }
 
